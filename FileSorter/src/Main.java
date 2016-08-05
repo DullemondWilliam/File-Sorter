f23b4194d69951af94da56de66e453545388410d
DullemondWilliam <Dullemond.William@gmail.com>
2016-08-04 20:17:55 -0400

    Delete Empty Folders
    Finds empty Folders in the file structure and deletes them

--- FileSorter/src/Main.java
+++ FileSorter/src/Main.java
@@ -1,7 +1,5 @@
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.Path;
 import java.util.ArrayList;
 
 public class Main {
@@ -14,19 +12,22 @@ public class Main {
  
     	File file = new File("C:/Users/Ares/Music/William Music");
     	
-    	cleanDir(file);
+    	//cleanDuplicates(file);
     	//printDir(0,file);
+    	cleanEmptyFolders(file);
     	
-    	System.out.println("Duplicates songs: "+duplicates);
-    	System.out.println("Unique songs: "+allFiles.size());
+    	
+    	//System.out.println("Duplicates songs: "+duplicates);
+    	//System.out.println("Unique songs: "+allFiles.size());
     }
 
-    public static void cleanDir(File file){
+    
+    public static void cleanDuplicates(File file){
     	if(file.isDirectory()){
     		File subFiles[] = file.listFiles();
         	if(subFiles == null)	{return;}
         	for(File f : subFiles){
-        		cleanDir(f);
+        		cleanDuplicates(f);
         	}
         	
     	}else if(file.isFile()){
@@ -43,41 +44,7 @@ public class Main {
     		System.err.println(file.getName());
     	}
     }
-    
-    public static String standardName(File file){
-    	String name = file.getName();
-    	while(name.charAt(0) == 32 ||
-    			(name.charAt(0) <= 57 && 
-    			name.charAt(0) >= 48)){
-    		
-    		name = name.substring(1);
-    	}
-    	
-    	
-    	//File newFile = new File(file.getParent()+ "\\"+name);
-        //System.out.println(file.renameTo(newFile));
-    	///////////////////////////////////////////////////////////////////////
-    	if(name.length() < 6)return name;
-        name = name.substring(0,name.length()-5);
-    	while(name.charAt(name.length()-1) == 32 ||
-    			(name.charAt(name.length()-1) <= 57 &&
-    			name.charAt(name.length()-1) >= 48)){
-    		name = name.substring(0, name.length()-2);
-    	}
-    	return name;
-    }
-    
-    public static Boolean nameExists(ArrayList<String> array, File file){
-    	String name = standardName(file);
-    	for(String a : array){
-    		if(a.equals(name.toLowerCase())){
-    			return true;
-    		}
-    	}
-    	return false;
-    }
-    
-    
+
     public static void printDir(int depth, File file){
     	if(!file.isDirectory() || !file.canRead()){
     		return;
@@ -96,5 +63,60 @@ public class Main {
     		printDir(depth+1,subFiles[i]);
     	}
     }
+    
+    public static void cleanEmptyFolders(File file){
+    	if(!file.exists() || !file.isDirectory()){ return;}
+    	
+    	//System.out.println(file);
+    	
+    	File[] subFiles = file.listFiles();
+    	if(subFiles.length == 0){
+    		System.out.println("* "+file);
+    		file.delete();
+    		return;
+    	}
+    	
+    	for(File f : subFiles){
+    		cleanEmptyFolders(f);
+    	}
+
+    	subFiles = file.listFiles();
+    	if(subFiles.length == 0){
+    		System.out.println(file);
+    		file.delete();
+    		return;
+    	}
+    }
+    
+    ///////////////////////////////////////////////////////////////////////////
+    public static String standardName(File file){
+    	String name = file.getName();
+    	while(name.charAt(0) == 32 ||
+    			(name.charAt(0) <= 57 && 
+    			name.charAt(0) >= 48)){
+    		
+    		name = name.substring(1);
+    	}    	
+    	if(name.length() < 6)return name;
+        name = name.substring(0,name.length()-5);
+    	while(name.charAt(name.length()-1) == 32 ||
+    			(name.charAt(name.length()-1) <= 57 &&
+    			name.charAt(name.length()-1) >= 48)){
+    		name = name.substring(0, name.length()-2);
+    	}
+    	return name;
+    }   
+   
+    public static Boolean nameExists(ArrayList<String> array, File file){
+    	String name = standardName(file);
+    	for(String a : array){
+    		if(a.equals(name.toLowerCase())){
+    			return true;
+    		}
+    	}
+    	return false;
+    }
+
+    
 
 }
